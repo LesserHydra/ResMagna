294d3ad0a2a56435bab22553737f872dfbdcf523
Justin Lawen <Justin@LesserHydra.com>
2015-11-24 18:36:59 -0600

    Clean up give subcommand and drop item on ground if inventory is full

--- PraedaGrandis/src/com/roboboy/PraedaGrandis/Commands/PGGiveSubCommand.java
+++ PraedaGrandis/src/com/roboboy/PraedaGrandis/Commands/PGGiveSubCommand.java
@@ -2,6 +2,7 @@ package com.roboboy.PraedaGrandis.Commands;
 
 import java.text.Collator;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import org.bukkit.Bukkit;
@@ -9,6 +10,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import com.roboboy.PraedaGrandis.PraedaGrandis;
 import com.roboboy.PraedaGrandis.Configuration.GrandItem;
 
@@ -16,15 +18,23 @@ public class PGGiveSubCommand implements SubCommand
 {
 	@Override
 	public void execute(CommandSender sender, Command command, String label, String[] args) {
-		if (args.length < 3) return;
+		//Parse arguments
+		if (args.length != 3) return;
 		Player givePlayer = Bukkit.getPlayer(args[1]);
 		GrandItem giveItem = PraedaGrandis.plugin.itemHandler.getItem(args[2]);
-		if (givePlayer != null && giveItem != null) {
-			givePlayer.getInventory().addItem(giveItem.create());
-			//TODO: Drop on ground if inventory full
-		}
-		else {
+		
+		//Return if player or item does not exist
+		if (givePlayer == null || giveItem == null) {
 			sender.sendMessage(ChatColor.RED + "Item or player does not exist.");
+			return;
+		}
+		
+		//Give to player
+		HashMap<Integer, ItemStack> remainingItems = givePlayer.getInventory().addItem(giveItem.create());
+		
+		//Drop on ground if inventory full
+		for (ItemStack item : remainingItems.values()) {
+			givePlayer.getWorld().dropItem(givePlayer.getLocation(), item);
 		}
 	}
 
