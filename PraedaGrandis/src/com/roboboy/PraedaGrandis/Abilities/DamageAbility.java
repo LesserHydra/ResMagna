caa88209420a6c5f2df28e8d441b9473e320f9a8
Justin Lawen <Justin@LesserHydra.com>
2015-12-01 12:27:33 -0600

    Improve damage ability
    Stop ignoring armor
    Send damage event
    Make damage come from holder

--- PraedaGrandis/src/com/roboboy/PraedaGrandis/Abilities/DamageAbility.java
+++ PraedaGrandis/src/com/roboboy/PraedaGrandis/Abilities/DamageAbility.java
@@ -1,5 +1,9 @@
 package com.roboboy.PraedaGrandis.Abilities;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import com.roboboy.PraedaGrandis.Abilities.Targeters.Target;
 import com.roboboy.PraedaGrandis.Abilities.Targeters.Targeter;
 import com.roboboy.PraedaGrandis.Configuration.ConfigString;
@@ -7,15 +11,37 @@ import com.roboboy.PraedaGrandis.Configuration.ConfigString;
 public class DamageAbility extends Ability
 {
 	final private double damageAmount;
+	final private boolean fromHolder;
+	final private DamageCause cause;
 	
 	public DamageAbility(ItemSlotType slotType, ActivatorType activator, Targeter targeter, ConfigString args) {
 		super(slotType, activator, targeter);
 		damageAmount = Double.parseDouble(args.get(1));
+		fromHolder = Boolean.parseBoolean(args.get(2));
+		cause = DamageCause.valueOf(args.get(3).toUpperCase());
 	}
-
+	
 	@Override
 	protected void execute(Target target) {
-		target.get().damage(damageAmount);
+		EntityDamageEvent event = createDamageEvent(target);
+		
+		Bukkit.getServer().getPluginManager().callEvent(event);
+		target.get().setLastDamageCause(event);
+		
+		if (!event.isCancelled()) damageTarget(target, event.getFinalDamage());
 	}
-
+	
+	@SuppressWarnings("deprecation")
+	private EntityDamageEvent createDamageEvent(Target target) {
+		if (fromHolder) return new EntityDamageByEntityEvent(target.getHolder(), target.get(), cause, damageAmount);
+		return new EntityDamageEvent(target.get(), cause, damageAmount);
+	}
+	
+	private void damageTarget(Target target, double amount) {
+		if (fromHolder) {
+			target.get().damage(amount, target.getHolder());
+			return;
+		}
+		target.get().damage(amount);
+	}
 }
