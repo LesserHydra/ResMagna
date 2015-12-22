33ac386bd809aace727d317a15d642ec5b7198a0
Justin Lawen <Justin@LesserHydra.com>
2015-12-21 23:55:10 -0600

    Begin allowing updates to grand inventory

--- PraedaGrandis/src/com/roboboy/PraedaGrandis/InventoryHandler.java
+++ PraedaGrandis/src/com/roboboy/PraedaGrandis/InventoryHandler.java
@@ -6,9 +6,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import com.roboboy.PraedaGrandis.Abilities.ItemSlotType;
 import com.roboboy.PraedaGrandis.Configuration.GrandItem;
 
@@ -63,6 +66,49 @@ public class InventoryHandler implements Listener
 		}
 	}
 	
+	//Player 
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onInventoryClick(InventoryClickEvent e)
+	{
+		if (!(e.getWhoClicked() instanceof Player)) return; //Is this even possible?
+		if (!e.getWhoClicked().getInventory().equals(e.getClickedInventory()) && e.getAction() != InventoryAction.MOVE_TO_OTHER_INVENTORY) return;
+		if (e.getAction() == InventoryAction.NOTHING || e.getAction() == InventoryAction.CLONE_STACK || e.getAction() == InventoryAction.UNKNOWN) return;
+		
+		final Player p = (Player) e.getWhoClicked();
+		new BukkitRunnable() { @Override public void run() {
+			registerPlayer(p);
+		}}.runTaskLater(plugin, 1);
+		/*if (!e.getWhoClicked().getInventory().equals(e.getClickedInventory())) return;
+		if (e.getAction() == InventoryAction.UNKNOWN || e.getAction() == InventoryAction.NOTHING || e.getAction() == InventoryAction.CLONE_STACK) return;
+		
+		GrandItem currentGrandItem = plugin.itemHandler.matchItem(e.getCurrentItem());
+		GrandItem cursorGrandItem = plugin.itemHandler.matchItem(e.getCursor());
+		ItemSlotType clickedSlotType = ItemSlotType.getSlotType(e.getSlotType(), e.getSlot(), e.getWhoClicked().getInventory().getHeldItemSlot());
+		if (currentGrandItem == null && cursorGrandItem == null) return;
+		if (clickedSlotType.isNull()) return;
+		
+		GrandInventory gInv = playerInventories.get(e.getWhoClicked().getName());
+		
+		//Current item is removed from inventory
+		if (currentGrandItem != null && (
+				e.getAction() == InventoryAction.COLLECT_TO_CURSOR || e.getAction() == InventoryAction.DROP_ALL_SLOT
+				|| e.getAction() == InventoryAction.DROP_ONE_SLOT || e.getAction() == InventoryAction.HOTBAR_MOVE_AND_READD
+				|| e.getAction() == InventoryAction.HOTBAR_SWAP || e.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY
+				|| e.getAction() == InventoryAction.PICKUP_ALL || e.getAction() == InventoryAction.PICKUP_HALF
+				|| e.getAction() == InventoryAction.PICKUP_ONE || e.getAction() == InventoryAction.PICKUP_SOME
+				|| e.getAction() == InventoryAction.SWAP_WITH_CURSOR)
+				) {
+			gInv.removeItem(currentGrandItem, clickedSlotType);
+		}
+		
+		//Cursor item is added to inventory
+		if (cursorGrandItem != null && (
+				e.getAction() == InventoryAction.PLACE_ALL || e.getAction() == InventoryAction.PLACE_ONE
+				|| e.getAction() == InventoryAction.PLACE_SOME || e.getAction() == InventoryAction.SWAP_WITH_CURSOR)) {
+			gInv.removeItem(cursorGrandItem, clickedSlotType);
+		}*/
+	}
+	
 	private void registerPlayer(Player p) {
 		GrandInventory gInv = new GrandInventory();
 		playerInventories.put(p.getName(), gInv);
@@ -75,5 +121,8 @@ public class InventoryHandler implements Listener
 			}
 		}
 	}
+	
+	//TODO: InventoryDragEvent, InventoryPickupItemEvent
+	//TODO: Others as needed
 	
 }
