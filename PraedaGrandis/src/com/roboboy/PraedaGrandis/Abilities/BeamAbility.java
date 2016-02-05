d2c8fbea67066366171e0dbfea372c484e3ce071
Justin Lawen <Justin@LesserHydra.com>
2016-02-05 14:12:11 -0600

    Add entity colliding to beams
    Add options to disable block or entity collision
    Other minor stuff

--- PraedaGrandis/src/com/roboboy/PraedaGrandis/Abilities/BeamAbility.java
+++ PraedaGrandis/src/com/roboboy/PraedaGrandis/Abilities/BeamAbility.java
@@ -2,6 +2,7 @@ package com.roboboy.PraedaGrandis.Abilities;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -23,9 +24,16 @@ public class BeamAbility extends Ability
 	private final double maxDistance;	//Maximum traversal distance
 	private final long maxTicks;		//Maximum time running
 	
+	private final boolean ignoreBlocks;
+	private final boolean ignoreEntities;
+	
+	private final double spreadX;
+	private final double spreadY;
+	private final double spreadZ;
+	
 	private final FunctionRunner onStep;
 	private final FunctionRunner onHitBlock;
-	//private final FunctionRunner onHitEntity;
+	private final FunctionRunner onHitEntity;
 	private final FunctionRunner onEnd;
 	
 	private final GrandLocation originLocation;
@@ -39,12 +47,20 @@ public class BeamAbility extends Ability
 		maxDistance = args.getDouble("maxdistance", 50D, false);
 		maxTicks = args.getLong("maxticks", 200L, false);
 		
+		ignoreBlocks = args.getBoolean("ignoreblocks", true, false);
+		ignoreEntities = args.getBoolean("ignoreentities", true, false);
+		
+		double spread = args.getDouble("spread", 0.5, false);
+		spreadX = args.getDouble("spreadx", spread, false);
+		spreadY = args.getDouble("spready", spread, false);
+		spreadZ = args.getDouble("spreadz", spread, false);
+		
 		originLocation = args.getLocation("originlocation", new GrandLocation(), false);
 		targetLocation = args.getLocation("targetlocation", new GrandLocation(), false);
 		
 		onStep = new FunctionRunner(args.get("onstep", null, false));
 		onHitBlock = new FunctionRunner(args.get("onhitblock", null, false));
-		//onHitEntity = new FunctionRunner(args.get("onhitentity", null, false));
+		onHitEntity = new FunctionRunner(args.get("onhitentity", null, false));
 		onEnd = new FunctionRunner(args.get("onend", null, false));
 	}
 
@@ -54,6 +70,7 @@ public class BeamAbility extends Ability
 	}
 	
 	private class BeamTimer extends BukkitRunnable {
+		private final LivingEntity shooter;
 		private final Vector velocity;
 		
 		private LivingEntity marker;
@@ -62,10 +79,11 @@ public class BeamAbility extends Ability
 		private double totalTicks = 0;
 		
 		BeamTimer(Target target) {
+			shooter = target.get();
 			Location startLocation = originLocation.calculate(target);
 			velocity = targetLocation.calculateDirection(target, startLocation).normalize().multiply(speed);
 			
-			marker = MarkerBuilder.buildMarker(startLocation);
+			marker = MarkerBuilder.buildInstantMarker(startLocation);
 			beamTarget = target;
 			
 			runTaskTimer(PraedaGrandis.plugin, 0L, delay);
@@ -74,44 +92,56 @@ public class BeamAbility extends Ability
 		@Override
 		public void run() {
 			//Update, one step at a time
-			boolean done = false;
-			for (int i = 0; i < numSteps && !done; i++) {
+			for (int i = 0; i < numSteps; i++) {
 				stepBeam();
-				done = checkForEnd();
+				if (checkForEnd()) {
+					stopBeam();
+					return;
+				}
 			}
-			
-			totalTicks += delay;
 			//Finish beam
-			if (done || totalTicks >= maxTicks) stopBeam();
+			totalTicks += delay;
+			if (totalTicks >= maxTicks) stopBeam();
 		}
 
 		private void stepBeam() {
 			//Move beam
-			marker = MarkerBuilder.buildMarker(marker.getLocation().add(velocity));
+			marker = MarkerBuilder.buildInstantMarker(marker.getLocation().add(velocity)); //TODO: Need more efficient system
 			beamTarget = beamTarget.target(marker);
 			totalDistance += speed;
 			//Run onStep
 			onStep.run(beamTarget);
 		}
 		
-		public boolean checkForEnd(){
-			if (hasHitBlock()) {
-				onHitBlock.run(beamTarget);
-				return true;
-			}
-			if (totalDistance >= maxDistance) return true;
-			return false;
-		}
-		
-		private boolean hasHitBlock() {
-			Material blockType = marker.getLocation().getBlock().getType();
-			return blockType.isSolid();
-		}
-
 		private void stopBeam() {
 			marker.remove();
 			onEnd.run(beamTarget);
 			cancel();
+		}
+		
+		public boolean checkForEnd() {
+			if (!ignoreEntities && hitEntity()) return true;
+			if (!ignoreBlocks && hitBlock()) return true;
+			if (totalDistance >= maxDistance) return true;
+			return false;
+		}
+		
+		private boolean hitBlock() {
+			Material blockType = marker.getLocation().getBlock().getType();
+			if (!blockType.isSolid()) return false;
+			onHitBlock.run(beamTarget);
+			return true;
+		}
+		
+		private boolean hitEntity() {
+			boolean hit = false;
+			for (Entity entity : marker.getNearbyEntities(spreadX, spreadY, spreadZ)) {
+				if (entity.equals(shooter)) continue;
+				if (!(entity instanceof LivingEntity)) continue;
+				onHitEntity.run(beamTarget.target((LivingEntity) entity));
+				hit = true;
+			}
+			return hit;
 		}
 		
 	}
