743f69048e97423c2dad7baa3f8e2ee5ae9815c6
Justin Lawen <Justin@LesserHydra.com>
2016-02-26 13:29:49 -0600

    Clean up RandomCollection
    Remove iterating functionality

--- PraedaGrandis/src/com/roboboy/PraedaGrandis/Configuration/RandomCollection.java
+++ PraedaGrandis/src/com/roboboy/PraedaGrandis/Configuration/RandomCollection.java
@@ -1,63 +1,61 @@
 package com.roboboy.PraedaGrandis.Configuration;
 
-import java.util.Iterator;
 import java.util.NavigableMap;
 import java.util.Random;
 import java.util.TreeMap;
 
-public class RandomCollection<E> implements Iterable<E>
+/**
+ * Represents a collection of elements with weighted probability.
+ *
+ * @param <E>
+ */
+public class RandomCollection<E>
 {
-	private final NavigableMap<Double, E> map = new TreeMap<Double, E>();
+	private final NavigableMap<Double, E> map = new TreeMap<>();
 	private final Random random;
 	private double total = 0;
 	
+	/**
+	 * Constructs a new RandomCollection with a new Random instance.
+	 */
 	public RandomCollection() {
 		this(new Random());
 	}
 	
+	/**
+	 * Constructs a new RandomCollection with a given Random instance.
+	 */
 	public RandomCollection(Random random) {
 		this.random = random;
 	}
 	
+	/**
+	 * Adds an element to the collection.
+	 * @param weight How likely this element is to be chosen reletive to others
+	 * @param result The element to add
+	 */
 	public void add(double weight, E result) {
 		if (weight <= 0) throw new IllegalArgumentException("Weight must be positive.");
 		total += weight;
 		map.put(total, result);
 	}
 	
+	/**
+	 * Gets a random element from the collection.
+	 * @return A random element
+	 */
 	public E next() {
 		if (map.isEmpty()) throw new IllegalStateException("Cannot get from an empty RandomCollection.");
 		double value = random.nextDouble() * total;
 		return map.ceilingEntry(value).getValue();
 	}
 	
+	/**
+	 * Checks if there are no elements.
+	 * @return True if collection is empty
+	 */
 	public boolean isEmpty() {
 		return map.isEmpty();
-	}
-	
-	@Override
-	public Iterator<E> iterator() {
-		return new RandomCollectionIterator();
-	}
-	
-	private class RandomCollectionIterator implements Iterator<E>
-	{
-		Iterator<E> backingIterator = map.values().iterator();
-		
-		@Override
-		public boolean hasNext() {
-			return backingIterator.hasNext();
-		}
-		
-		@Override
-		public E next() {
-			return backingIterator.next();
-		}
-		
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException("Cannot remove from a RandomCollection.");
-		}
 	}
 	
 }
